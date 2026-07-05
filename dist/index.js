5e8afe50d0f667ff3131028188b6f3c4474f66be
Jefferson Vargas <iovargasespinozajeff@gmail.com>
2026-07-05 11:54:26 -0500

    Make built action standalone

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,14 @@
-const core = require("@actions/core");
-
 async function run() {
   const joke = "Why do developers prefer dark mode? Because light attracts bugs.";
-  core.setOutput("joke", joke);
+  if (process.env.GITHUB_OUTPUT) {
+    require("fs").appendFileSync(process.env.GITHUB_OUTPUT, `joke=${joke}\n`);
+  }
   console.log(joke);
 }
 
-run().catch((error) => core.setFailed(error.message));
+run().catch((error) => {
+  console.error(error.message);
+  process.exit(1);
+});
 
 // bundled with ncc build src/main.js -o dist
